cf5593ea5d0e6f6acd6d17fd5b2fc31da7214f28
Maxime Moison <maximaxof@me.com>
2017-11-05 17:23:42 -0800

    needs website structure

--- app.py
+++ app.py
@@ -3,7 +3,17 @@ app = Flask(__name__)
  
 @app.route("/")
 def home():
-    return render_template('index.html')
+	return render_template('index.html')
  
 if __name__ == "__main__":
-    app.run()
+	app.run()
+
+@app.route("/", methods=['GET', 'POST'])
+def loadSchedules():
+	print 'hello'
+	if request.method == 'POST':
+		from Scheduler import findSchedules
+		return render_template('index.html')
+		
+	else:
+		return render_template('index.html')
