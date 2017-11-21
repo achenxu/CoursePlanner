53bf4654d09272591c13f6cd3ba11a0c94b65c1a
Maxime Moison <maximaxof@me.com>
2017-11-21 11:17:59 -0800

    Commenting

--- Scheduler.py
+++ Scheduler.py
@@ -1,18 +1,8 @@
 import os.path, time, csv
 from Functions import extractValidSchedules, fetchTimes
 
-# Download latest version of the page and check if it changed
+# Find schedules
 def findSchedules(coursesToExtract):
-	if not os.path.isfile("testfiles/result.csv"):
-	    if os.path.isfile("testfiles/long.html"):
-		import Parser
-	    else:
-		quit()
-	else:
-	    if os.path.isfile("testfiles/long.html"):
-		if os.path.getmtime("testfiles/long.html") > os.path.getmtime("testfiles/result.csv"):
-		    import Parser
-
 	with open("testfiles/result.csv", "r") as f:
 	    reader = csv.reader(f)
 	    data = list(reader)
@@ -26,5 +16,14 @@ def findSchedules(coursesToExtract):
 
 	return extractValidSchedules(data, coursesToExtract, classIDs)
 
-
-
+# Download latest version of the page and check if it changed
+def updateDB:
+    if not os.path.isfile("testfiles/result.csv"):
+        if os.path.isfile("testfiles/long.html"):
+        import Parser
+        else:
+        return
+    else:
+        if os.path.isfile("testfiles/long.html"):
+        if os.path.getmtime("testfiles/long.html") > os.path.getmtime("testfiles/result.csv"):
+            import Parser
